8c9aeb53f499a433adfe7452ebe22eeb9bab2f2b
Basha Mujavarsheik <110763051+mskbasha@users.noreply.github.com>
2023-08-25 22:21:49 +0530

    create py changed to use seperately without command line

--- create_captions.py
+++ create_captions.py
@@ -9,12 +9,12 @@ from lavis.models import load_model_and_preprocess
 
 class caption:
     
-    def __init__(self, device : torch.device) -> None:
+    def __init__(self, device : torch.device, gm_loc : str) -> None:
         self.device = device
-        
+        self.load_models(gm_loc)
     def load_models(self,gm_loc : str):
         
-        model = GMFlow(feature_channels = 128,
+        self.model = GMFlow(feature_channels = 128,
                         num_scales = 1,
                         upsample_factor = 8,
                         num_head = 1,
@@ -24,16 +24,15 @@ class caption:
                         ).to(self.device)
         checkpoint = torch.load(gm_loc, map_location= self.device)
         weights = checkpoint['model'] if 'model' in checkpoint else checkpoint
-        model.load_state_dict(weights, strict='store_true')
-        model_blip, vis_processors, _ = load_model_and_preprocess(name="blip2_t5", 
+        self.model.load_state_dict(weights, strict='store_true')
+        self.model_blip, self.vis_processors, _ = load_model_and_preprocess(name="blip2_t5", 
                                                                 model_type="pretrain_flant5xxl", 
                                                                 is_eval=True, 
                                                                 device=self.device)
-        return model,model_blip,vis_processors
     
-    def captions(self, video_loc :str , gm_loc : str,vel = 60):
+    def captions(self, video_loc :str ,vel = 60) :
         
-        model,model_blip,vis_processors = self.load_models(self.device,gm_loc)
+        
         video = cv.VideoCapture(video_loc)
         prev_frames,next_frames,framesForCaptions,times,goodframes = [],[],[],[],[]
         pb = progressbar(1)
@@ -69,7 +68,7 @@ class caption:
                 if count==batch_size:
                     image1 = torch.stack(prev_frames).float()
                     image2 = torch.stack(next_frames).float()
-                    results_dict = model(image1.to(self.device), image2.to(self.device),
+                    results_dict = self.model(image1.to(self.device), image2.to(self.device),
                                             attn_splits_list=[2],
                                             corr_radius_list=[-1],
                                             prop_radius_list=[-1],
@@ -78,14 +77,14 @@ class caption:
                     velocity = results_dict['flow_preds'][-1]
                     velocity = torch.sqrt(velocity[:,0]**2+velocity[:,1]**2)
                     velocity = torch.sum(velocity,axis=[1,2])/sum(velocity.shape[1:])>vel
-                    torch.cuda.empty_cache()
+                    #torch.cuda.empty_cache()
                     for ind,i in enumerate(velocity):
                         if i:
                             frame_0 = Image.fromarray(np.flip(framesForCaptions[ind],axis = -1))
-                            image = vis_processors["eval"](frame_0).unsqueeze(0).to(self.device)
-                            goodframes.append([model_blip.generate({"image": image, 
+                            image = self.vis_processors["eval"](frame_0).unsqueeze(0).to(self.device)
+                            goodframes.append([self.model_blip.generate({"image": image, 
                                                                     "prompt": "Caption this image"}),times[ind]])
                     count = 0
-                    prev_frames,next_frames,framesForCaptions,times = [],[],[],[],[]
+                    prev_frames,next_frames,framesForCaptions,times = [],[],[],[]
         torch.cuda.empty_cache()
         return goodframes
